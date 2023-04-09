25f80c83034dbc61bcbf1ca84327f90d8858957e
Charlie Taylor <peekoecode@gmail.com>
2023-04-08 17:19:42 -0700

    database with get and insert and routes :) 🗿

---
+++ api/db.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+con = sqlite3.connect("qc.db")
+
+QUESTIONS = "CREATE TABLE IF NOT EXISTS questions (id INTEGER PRIMARY KEY, question TEXT, answer TEXT)"
+CHOICES = "CREATE TABLE IF NOT EXISTS choices (id INTEGER PRIMARY KEY, question_id INTEGER,answer TEXT)"
+
+# KEEP IN CRUD ORDER
+
+
+def create_schema():
+    cur = con.cursor()
+    cur.execute(QUESTIONS)
+    cur.execute(CHOICES)
+    con.commit()
+
+
+def insert_question(question, answer) -> bool:
+    cur = con.cursor()
+    cur.execute(
+        "INSERT INTO questions (question, answer) VALUES (?, ?)", (question, answer)
+    )
+    con.commit()
+    return cur.rowcount == 1
+
+
+def get_question_by_id(id):
+    cur = con.cursor()
+    cur.execute("SELECT * FROM questions WHERE id = ?", (id,))
+    return cur.fetchone()
+
+
+def insert_choice(question_id, choice) -> bool:
+    cur = con.cursor()
+    cur.execute(
+        "INSERT INTO choices (question_id, answer) VALUES (?, ?)", (question_id, choice)
+    )
+    con.commit()
+    return cur.rowcount == 1
+
+def get_choice_by_id(id):
+    cur = con.cursor()
+    cur.execute("SELECT * FROM choices WHERE id = ?", (id,))
+    return cur.fetchone()
+
--- api/main.py
+++ api/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from assistant import Assistant
 from fastapi.middleware.cors import CORSMiddleware
 
+from db import (
+    create_schema,
+    insert_question,
+    get_question_by_id,
+    insert_choice,
+    get_choice_by_id,
+)
 
 app = FastAPI()
 
@@ -15,6 +22,12 @@ app.add_middleware(
 )
 
 
+app = FastAPI()
+
+# create tables on startup
+create_schema()
+
+
 @app.get("/")
 async def root():
     return {"message": "gpt showdown :)"}
@@ -23,3 +36,35 @@ async def root():
 @app.get("/get_gpt_response")
 async def get_gpt_response():
     pass
+
+
+@app.get("/api/question/{id}")
+async def get_question(id: int):
+    return get_question_by_id(id)
+
+
+@app.get("/api/choice/{id}")
+async def get_choice(id: int):
+    return get_choice_by_id(id)
+
+
+@app.post("/api/question")
+async def post_question(question: str, answer: str):
+    q = insert_question(question, answer)
+
+    if q:
+      return {"message": "question added"}
+
+    raise HTTPException(status_code=400, detail="Failed to add question")
+        
+
+
+
+@app.post("/api/choice")
+async def post_choice(question_id: int, choice: str):
+    c = insert_choice(question_id, choice)
+    
+    if c:
+      return {"message": f"choice added to quesion {question_id}"}
+    
+    raise HTTPException(status_code=400, detail="Failed to add choice")
