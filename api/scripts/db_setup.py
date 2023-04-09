e794d3038eed5ddc3261fc5430cb298a03408ac2
Justin Stitt <jstitt007@gmail.com>
2023-04-09 14:11:11 -0700

    fix db gen

--- api/scripts/db_setup.py
+++ api/scripts/db_setup.py
@@ -10,42 +10,57 @@ def insert_quizzes():
     categories = [x[:-1] for x in re.findall(r".+:", raw) if "Answer" not in x]
     questions = re.findall(r"\d\. .*", raw)
 
+    choices = re.findall(r"[A-Z]\).*", raw)
+    assert len(choices) == len(questions) * 5, "Improper Choice to Question ratio"
+
     category_to_questions: dict[str, list[str]] = defaultdict(lambda: list())
     category = -1
-    for question in questions:
+    actual_choices = []
+    answers = []
+    for i, question in enumerate(questions):
         if "1." in question:
             category += 1
             # go next category
         category_to_questions[categories[category]].append(question)
-
-    choices = re.findall(r"[A-Z]\).*", raw)
-    assert len(choices) == len(questions) * 5, "Improper Choice to Question ratio"
-
-    quizzes = []
-    # print(choices)
-    for i, category in enumerate(categories):
-        quiz_name = category.lower() + "-1"
-        # print(i, category)
-        answer = choices[(i * 5) + 4][0]
-        raw_choices = choices[i * 4 : i * 4 + 4]
-        actual_choices = []
+        raw_choices = choices[i * 5 : i * 5 + 4]
+        correct_choice = choices[i * 5 + 4]
+        answers.append(correct_choice)
         for choice in raw_choices:
             actual_choices.append(CreateChoice(choice=choice[0], value=choice[3:]))
 
-        question_list = [
-            CreateQuestion(question=x, answer=answer, choices=actual_choices)
-            for x in category_to_questions[category]
+    quizzes = []
+    # print(choices)
+    # print(answers)
+    # print(len(answers))
+    # print(actual_choices)
+    # print(len(questions), " ", (len(actual_choices)))
+
+    for cidx, category in enumerate(categories):
+        quiz_name = category.lower() + "-1"
+        questions_for_category = category_to_questions[category]
+        answer_for_questions = [answers[i] for i in range(len(questions_for_category))]
+        choices_for_question = [
+            actual_choices[i * 4 : i * 4 + 4]
+            for i in range(len(questions_for_category))
         ]
 
-        quiz = CreateQuiz(name=quiz_name, category=category, questions=question_list)
-        quizzes.append(quiz)
-        quiz_id = insert_quiz(quiz)
-        assert quiz_id is not None, "Error: Invalid Quiz ID"
+        # print(
+        #     f"{questions_for_category=}, {answer_for_questions=}, {choices_for_question}\n"
+        # )
+        questions_list = []
+        for i, _question in enumerate(questions_for_category):
+            new_question = CreateQuestion(
+                question=_question, answer=answers[i], choices=choices_for_question[i]
+            )
+            questions_list.append(new_question)
 
-        for question_to_add in question_list:
-            question_id = insert_question(quiz_id=quiz_id, q=question_to_add)
-            assert question_id is not None, "Error: invalid question_id"
-            for choice in actual_choices:
+        quiz = CreateQuiz(name=quiz_name, category=category, questions=questions_list)
+        quiz_id = insert_quiz(quiz)
+
+        # print(questions_list)
+        for i, _question in enumerate(questions_list):
+            question_id = insert_question(quiz_id=quiz_id, q=_question)
+            for choice in choices_for_question[i]:
                 insert_choice(question_id=question_id, c=choice)
 
     return quizzes
@@ -55,5 +70,3 @@ if __name__ == "__main__":
     create_schema()
     quizzes = insert_quizzes()
     # print(quizzes)
-    for quiz in quizzes:
-        print(quiz)
